c191bb129625594509b4ffaa71d49fa609c4baea
lenthedev <your_email_address@example.com>
2020-11-08 03:36:43 -0500

    Task:"Added Set Up Method For Instantiation And Testing"

--- src/CalculatorTest.py
+++ src/CalculatorTest.py
@@ -2,27 +2,27 @@ import unittest
 from Calculator import Calculator
 
 class MyTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+
     # Test for the constructor
     def test_instantiate_calculator(self):
-        calculator = Calculator()
-        self.assertIsInstance(calculator, Calculator)
+        self.assertIsInstance(self.calculator, Calculator)
 
     # Test for the result property
     def test_results_property_calculator(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.result, 0)
+        self.assertEqual(self.calculator.result, 0)
 
     # Test for the result of addition
     def test_add_method_calculator(self):
-        calculator = Calculator()
         self.assertEqual(calculator.add(2, 2), 4)
-        self.assertEqual(calculator.result, 6)
+        self.assertEqual(self.calculator.result, 6)
 
     # Test for the result of subtraction
     def test_subtract_method_calculator(self):
-        calculator = Calculator()
         self.assertEqual(calculator.subtract(10, 7), 3)
-        self.assertEqual(calculator.result, 3)
+        self.assertEqual(self.calculator.result, 3)
 
         if __name__ == '__main__':
             unittest.main()
